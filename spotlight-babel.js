fa60b3c66defe9206bde11685ef153f29d604017
Alvaro Montoro <alvaromontoro@gmail.com>
2018-08-16 08:41:17 -0500

    Initial upload
    This commit includes the original code from CodePen + compiled and minified versions for JS and CSS.

---
+++ spotlight-babel.js
@@ -0,0 +1,171 @@
+/** 
+SpotlightJS: a highlighting/instructions plugin for JS.
+Developed by Alvaro Montoro. More info: https://github.com/alvaromontoro/spotlightjs
+**/
+class SpotlightJS {
+
+  constructor(options) {
+    this.init = "";
+    this.shape = "round";
+    this.color = "rgba(0,0,0,0.4)";
+    this.skip = true;
+    this.skipText = "Skip presentation";
+    this.previous = true;
+    this.previousText = "Previous";
+    this.next = true;
+    this.nextText = "Next";
+    this.timer = 0;
+    this.current = 0;
+    this.steps = [];
+    this.frame = null;
+    this.spot = null;
+    this.text = null;
+    this.textContent = null;
+    this.previousButton = null;
+    this.nextButton = null;
+    
+    // extend the options that can be customized
+    if (options) {
+      if (options.init) { this.init = options.init; }
+      if (options.shape) { this.shape = options.shape == "square" ? "square" : "round"; }
+      if (options.hasOwnProperty("skip")) { this.skip = (options.skip == true); }
+      if (options.hasOwnProperty("previous")) { this.previous = (options.previous == true); }
+      if (options.hasOwnProperty("next")) { this.next = (options.next == true); }
+      if (options.skipText) { this.skipText = options.skipText; }
+      if (options.previousText) { this.previousText = options.previousText; }
+      if (options.nextText) { this.nextText = options.nextText; }
+      if (options.steps) { this.steps = options.steps; }
+    }
+    
+    // create a list of selectors for the elements to get the spotlight (if none exists yet)
+    if (this.init != "" && !this.hasSteps()) {
+      let el = this.init;
+      while (el) {
+        this.steps.push(el);
+        el = document.querySelector(el).dataset.spNext;
+      }
+    }
+    
+    // create the html structure needed for the spotlight (inline CSS, sorry)
+    let spInst = document.createElement("div");
+    spInst.id = "spjs-frame";
+    let spSpot = document.createElement("div");
+    spSpot.id = "spjs-spot";
+    spSpot.classList.add("spjs-" + this.shape);
+    let spText = document.createElement("div");
+    spText.id = "spjs-text";
+    let spTextContent = document.createElement("span");
+    spTextContent.id = "spjs-textContent";
+    spText.appendChild(spTextContent);
+    if (this.skip) {
+      let spSkip = document.createElement("span");
+      spSkip.innerHTML = this.skipText;
+      spSkip.id = "spjs-skip";
+      spSkip.addEventListener("click", this.stop);
+      spInst.appendChild(spSkip)
+    }
+    if (this.previous) {
+      let spPrev = document.createElement("span");
+      spPrev.innerHTML = this.previousText;
+      spPrev.addEventListener("click", this.goToPreviousStep);
+      spText.appendChild(spPrev);
+      this.previousButton = spPrev;
+    }
+    if (this.next) {
+      let spNext = document.createElement("span");
+      spNext.innerHTML = this.nextText;
+      spNext.addEventListener("click", this.goToNextStep);
+      spText.appendChild(spNext);
+      this.nextButton = spNext;
+    }
+    spSpot.appendChild(spText);
+    spInst.appendChild(spSpot);
+    document.querySelector("body").appendChild(spInst);
+    this.frame = spInst;
+    this.spot = spSpot;
+    this.text = spText;
+    this.textContent = spTextContent;
+    
+    // if window is resized of scrolled, recalculate
+    let $this = this;
+    window.addEventListener("resize", function() { $this.goToStep($this.current); });
+    window.addEventListener("scroll", function() { $this.goToStep($this.current); });
+  }
+
+  /** start with the first step of the instructions */
+  start = () => {
+    if (this.hasSteps()) {
+      this.frame.style.display = "block";
+      this.goToFirstStep();
+    }
+  }
+  
+  /** end the instructions: hide everything */
+  stop = () => {
+    this.frame.style.display = "none";
+  }
+  
+  /** go to the specified step in the instructions */
+  goToStep = (step) => {
+    if (this.hasSteps()) {
+      if (!isNaN(step) && step > -1 && step < this.steps.length) {
+        this.spot.classList.remove("spjs-step-"+this.current, "spjs-step-square", "spjs-step-round");
+        this.current = step;
+        let el = document.querySelector(this.steps[step]);
+        if (el) {
+          let elRect = el.getBoundingClientRect();
+          this.spot.style.width = (elRect.width + 20) + "px";
+          this.spot.style.height = (elRect.height + 20) + "px";
+          this.spot.style.top = (elRect.top - 10) + "px";
+          this.spot.style.left = (elRect.left - 10) + "px";
+          this.spot.classList.add("spjs-step-" + step); // to allow user styling specific to each step
+          this.textContent.textContent = el.dataset.spText;
+          if (this.previousButton) 
+            this.previousButton.style.display = this.current == 0 ? "none" : "inline-block";
+          if (el.dataset.spShape)
+            this.spot.classList.add("spjs-step-" + el.dataset.spShape);
+        }
+      }
+    }
+  }
+  
+  /** go to the first step of the instructions */
+  goToFirstStep = () => {
+    this.goToStep(0);
+  }
+  
+  /** go to the next step of the instructions */
+  goToNextStep = () => {
+    if (this.current == this.steps.length - 1) {
+      this.stop();
+    }
+    if (this.current < this.steps.length) {
+      this.goToStep(this.current + 1);
+    }
+  }
+  
+  /** go to the next step of the instructions */
+  goToPreviousStep = () => {
+    if (this.current > 0) {
+      this.goToStep(this.current - 1);
+    }
+  }
+  
+  /** indicates if the instructions have any steps to show (as a whole) */
+  hasSteps = () => (this.init != "" && this.steps.length > 0);
+  
+  /** returns the identifier of the current element */
+  currentElementSelector = () => this.steps[this.current];
+  
+  /** returns the currently highlighted element */
+  currentElement = () => document.querySelector(this.currentElementSelector());
+  
+  /** returns the text of the currently highlighted element */
+  getText = () => this.currentElement().dataset.spText;
+  
+  /** returns the current step */
+  getStep = () => this.current + 1;
+
+  /** returns the total number of steps */
+  getTotalSteps = () => this.steps.length + 1;
+};
---
+++ spotlight.js
@@ -0,0 +1,206 @@
+"use strict";
+
+/** 
+SpotlightJS: a highlighting/instructions plugin for JS
+Developed by Alvaro Montoro (alvaromontoro@gmail.com)
+More info: https://github.com/alvaromontoro/spotlightjs
+**/
+var SpotlightJS = function SpotlightJS(options) {
+  var _this = this;
+
+	// starts the spotlight from the first step
+  this.start = function () {
+    if (_this.hasSteps()) {
+      _this.frame.style.display = "block";
+      _this.goToFirstStep();
+    }
+  };
+
+  // stops the spotlight
+  this.stop = function () {
+    _this.frame.style.display = "none";
+  };
+
+  // animates to the indicated step
+  this.goToStep = function (step) {
+    if (_this.hasSteps()) {
+      if (!isNaN(step) && step > -1 && step < _this.steps.length) {
+        _this.spot.classList.remove("spjs-step-" + _this.current, "spjs-step-square", "spjs-step-round");
+        _this.current = step;
+        var el = document.querySelector(_this.steps[step]);
+        if (el) {
+          var elRect = el.getBoundingClientRect();
+          _this.spot.style.width = elRect.width + 20 + "px";
+          _this.spot.style.height = elRect.height + 20 + "px";
+          _this.spot.style.top = elRect.top - 10 + "px";
+          _this.spot.style.left = elRect.left - 10 + "px";
+          _this.spot.classList.add("spjs-step-" + step); // to allow user styling specific to each step
+          _this.textContent.textContent = el.dataset.spText;
+          if (_this.previousButton) _this.previousButton.style.display = _this.current == 0 ? "none" : "inline-block";
+          if (el.dataset.spShape) _this.spot.classList.add("spjs-step-" + el.dataset.spShape);
+        }
+      }
+    }
+  };
+
+  // animates to the first step
+  this.goToFirstStep = function () {
+    _this.goToStep(0);
+  };
+
+  // animates to the next step or ends spotlight if in last step
+  this.goToNextStep = function () {
+    if (_this.current == _this.steps.length - 1) {
+      _this.stop();
+    }
+    if (_this.current < _this.steps.length) {
+      _this.goToStep(_this.current + 1);
+    }
+  };
+
+  // animates back to the previous step
+  this.goToPreviousStep = function () {
+    if (_this.current > 0) {
+      _this.goToStep(_this.current - 1);
+    }
+  };
+
+  // indicates if the instructions have any step in them
+  this.hasSteps = function () {
+    return _this.init != "" && _this.steps.length > 0;
+  };
+
+  // returns the selector for the element that is being highlighted
+  this.currentElementSelector = function () {
+    return _this.steps[_this.current];
+  };
+
+  // returns the DOM element that is being highlighted at the moment
+  this.currentElement = function () {
+    return document.querySelector(_this.currentElementSelector());
+  };
+
+  // returns the text associated to the current step
+  this.getText = function () {
+    return _this.currentElement().dataset.spText;
+  };
+
+  // returns the current step (1..n)
+  this.getStep = function () {
+    return _this.current + 1;
+  };
+
+  // returns the total number of steps in the instructions
+  this.getTotalSteps = function () {
+    return _this.steps.length + 1;
+  };
+
+  // initial variables with default values
+  this.init = "";
+  this.shape = "round";
+  this.color = "rgba(0,0,0,0.4)";
+  this.skip = true;
+  this.skipText = "Skip presentation";
+  this.previous = true;
+  this.previousText = "Previous";
+  this.next = true;
+  this.nextText = "Next";
+  this.timer = 0;
+  this.current = 0;
+  this.steps = [];
+  this.frame = null;
+  this.spot = null;
+  this.text = null;
+  this.textContent = null;
+  this.previousButton = null;
+  this.nextButton = null;
+
+  // extend the options that can be customized
+  if (options) {
+    if (options.init) {
+      this.init = options.init;
+    }
+    if (options.shape) {
+      this.shape = options.shape == "square" ? "square" : "round";
+    }
+    if (options.hasOwnProperty("skip")) {
+      this.skip = options.skip == true;
+    }
+    if (options.hasOwnProperty("previous")) {
+      this.previous = options.previous == true;
+    }
+    if (options.hasOwnProperty("next")) {
+      this.next = options.next == true;
+    }
+    if (options.skipText) {
+      this.skipText = options.skipText;
+    }
+    if (options.previousText) {
+      this.previousText = options.previousText;
+    }
+    if (options.nextText) {
+      this.nextText = options.nextText;
+    }
+    if (options.steps) {
+      this.steps = options.steps;
+    }
+  }
+
+  // create a list of selectors for the elements to get the spotlight (if none exists yet)
+  if (this.init != "" && !this.hasSteps()) {
+    var el = this.init;
+    while (el) {
+      this.steps.push(el);
+      el = document.querySelector(el).dataset.spNext;
+    }
+  }
+
+  // create the html structure needed for the spotlight
+  var spInst = document.createElement("div");
+  spInst.id = "spjs-frame";
+  var spSpot = document.createElement("div");
+  spSpot.id = "spjs-spot";
+  spSpot.classList.add("spjs-" + this.shape);
+  var spText = document.createElement("div");
+  spText.id = "spjs-text";
+  var spTextContent = document.createElement("span");
+  spTextContent.id = "spjs-textContent";
+  spText.appendChild(spTextContent);
+  if (this.skip) {
+    var spSkip = document.createElement("span");
+    spSkip.innerHTML = this.skipText;
+    spSkip.id = "spjs-skip";
+    spSkip.addEventListener("click", this.stop);
+    spInst.appendChild(spSkip);
+  }
+  if (this.previous) {
+    var spPrev = document.createElement("span");
+    spPrev.innerHTML = this.previousText;
+    spPrev.addEventListener("click", this.goToPreviousStep);
+    spText.appendChild(spPrev);
+    this.previousButton = spPrev;
+  }
+  if (this.next) {
+    var spNext = document.createElement("span");
+    spNext.innerHTML = this.nextText;
+    spNext.addEventListener("click", this.goToNextStep);
+    spText.appendChild(spNext);
+    this.nextButton = spNext;
+  }
+  spSpot.appendChild(spText);
+  spInst.appendChild(spSpot);
+  document.querySelector("body").appendChild(spInst);
+  this.frame = spInst;
+  this.spot = spSpot;
+  this.text = spText;
+  this.textContent = spTextContent;
+
+  // if window is resized of scrolled, recalculate
+  var $this = this;
+  window.addEventListener("resize", function () {
+    $this.goToStep($this.current);
+  });
+  window.addEventListener("scroll", function () {
+    $this.goToStep($this.current);
+  });
+};
