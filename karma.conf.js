5da3e26e950dbedfa71b396ba2d34e39f454166b
Bruno Dulcetti <brunodulcetti@gmail.com>
2018-10-04 18:59:56 -0300

    change eslint change quotes

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,39 @@
 module.exports = config => {
   config.set({
     autoWatch: true,
-    basePath: "",
+    basePath: '',
     colors: true,
     concurrency: Infinity,
-    exclude: ["demos/**/*"],
-    files: ["spec.bundle.js"],
-    frameworks: ["jasmine"],
+    exclude: ['demos/**/*'],
+    files: ['spec.bundle.js'],
+    frameworks: ['jasmine'],
     plugins: [
-      "karma-chrome-launcher",
-      "karma-spec-reporter",
-      "karma-coverage",
-      "karma-jasmine",
-      "karma-threshold-reporter",
-      "karma-babel-preprocessor",
-      "karma-webpack"
+      'karma-chrome-launcher',
+      'karma-spec-reporter',
+      'karma-coverage',
+      'karma-jasmine',
+      'karma-threshold-reporter',
+      'karma-babel-preprocessor',
+      'karma-webpack'
     ],
     port: 9876,
     preprocessors: {
-      "./src/**/!(*.spec).js": ["coverage"],
-      "spec.bundle.js": ["webpack"]
+      './src/**/!(*.spec).js': ['coverage'],
+      'spec.bundle.js': ['webpack']
     },
     logLevel: config.LOG_INFO,
-    browsers: ["ChromeHeadless"],
+    browsers: ['ChromeHeadless'],
     thresholdReporter: {
       statements: 80,
       branches: 80,
       functions: 80,
       lines: 80
     },
-    reporters: ["spec", "threshold", "coverage"],
-    webpack: require("./webpack.karma.config"),
+    reporters: ['spec', 'threshold', 'coverage'],
+    webpack: require('./webpack.karma.config'),
     webpackMiddleware: {
       noInfo: true,
-      stats: "errors-only"
+      stats: 'errors-only'
     }
   });
 };
