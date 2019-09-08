bf2e500a5aa17087e0747fc4888028593891edcc
Alvaro Montoro <alvaromontoro@gmail.com>
2019-09-08 07:58:11 -0500

    Add tests
    - New tests to cover more

--- tests/tourguide.test.js
+++ tests/tourguide.test.js
@@ -1,8 +1,29 @@
 import TourGuide from '../src/tourguide';
+import { read } from 'fs';
 
 describe('TourGuide', () => {
-  test('Check gameControl gamepads', () => {
+  test('TourGuide is created with default values', () => {
     const tour = new TourGuide();
     expect(tour).not.toEqual(null);
+    expect(tour.shape).toEqual('round');
+  });
+
+  test('TourGuide created with custom values', () => {
+    const tour = new TourGuide({
+      color: read,
+      initTrigger: false,
+      next: false,
+      previous: false,
+      previousText: 'no previous text',
+      shape: 'square',
+      skip: false,
+      skipText: 'no text',
+      steps: {}
+    });
+    expect(tour).not.toEqual(null);
+    expect(tour.shape).toEqual('square');
+    expect(tour.skip).toEqual(false);
+    expect(tour.previous).toEqual(false);
+    expect(tour.next).toEqual(false);
   });
 });
